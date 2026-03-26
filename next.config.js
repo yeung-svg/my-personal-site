9ca27915e916867d9db13216de65ed3175b5c1cb
yeung-svg <coxongodownnc367@gmail.com>
2026-03-26 19:23:47 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,20 @@
 const isGithubPages = process.env.GITHUB_PAGES === "true";
+const hasGithubPagesCustomDomain = process.env.GITHUB_PAGES_CUSTOM_DOMAIN === "true";
+
 const repoName = process.env.GITHUB_REPO_NAME || "my-personal-site";
-const basePath = isGithubPages ? `/${repoName}` : "";
+const basePath = isGithubPages && !hasGithubPagesCustomDomain ? `/${repoName}` : "";
 
 const nextConfig = {
   ...(isGithubPages
     ? {
         output: "export",
-        basePath,
-        assetPrefix: basePath,
         trailingSlash: true,
+        ...(basePath
+          ? {
+              basePath,
+              assetPrefix: basePath,
+            }
+          : {}),
       }
     : {}),
   images: {
